64df43f1ba364d24370da9f06bd18c600b20bae3
Rifandye <rilfransyah@gmail.com>
2024-08-23 23:07:13 +0700

    feat: add get all events and by slug

---
+++ follow-point/src/app/api/events/[slug]/route.ts
@@ -0,0 +1,42 @@
+import { EventModel } from "@/app/db/models/event";
+import { NextResponse } from "next/server";
+
+export async function GET(
+  request: Request,
+  { params }: { params: { slug: string } }
+) {
+  try {
+    const slug = params.slug;
+
+    const result = await EventModel.getEventBySlug(slug);
+
+    if (!result) {
+      return NextResponse.json(
+        {
+          error: "Event not found",
+        },
+        {
+          status: 404,
+        }
+      );
+    }
+
+    return NextResponse.json(
+      {
+        data: result,
+      },
+      {
+        status: 200,
+      }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      {
+        error: "Internal Server Error",
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
